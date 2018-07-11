26455a3a4d133c17e2675e7592f4d281a208202b
Junyoung Clare Jang <jjc9310@gmail.com>
2018-07-10 23:52:01 -0400

    chore(package): add commitlint setting

---
+++ config/commitlint/commitlint.config.js
@@ -0,0 +1,62 @@
+module.exports = {
+  rules: {
+    'body-leading-blank': [
+      2,
+      'always',
+    ],
+    'footer-leading-blank': [
+      2,
+      'always',
+    ],
+    'header-max-length': [
+      2,
+      'always',
+      72,
+    ],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'repo',
+        'package',
+      ],
+    ],
+    'scope-empty': [
+      2,
+      'never',
+    ],
+    'scope-case': [
+      0,
+    ],
+    'subject-case': [
+      2,
+      'always',
+      'lowerCase',
+    ],
+    'subject-empty': [
+      0,
+    ],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'chore',
+        'docs',
+        'feat',
+        'fix',
+        'init',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+    'type-case': [
+      0,
+    ],
+    'type-empty': [
+      2,
+      'never',
+    ],
+  },
+};
