dd1ff48c37df8c7fac1bb60149e11ce06e615b50
Junyoung Clare Jang <jjc9310@gmail.com>
2018-07-26 01:02:28 -0400

    refactor(package): port test to use jest
    mocha (and its environments) does not work well
    with ES6 module syntax. So I changed it to use jest.
    However, ts-jest@23 does not work with extends of tsconfig.json,
    so package should temporalily use ts-jest@^22.

--- test/behaviour/runnable.ts
+++ test/behaviour/runnable.ts
@@ -1,11 +1,9 @@
-import { expect } from 'chai';
-
-import { main } from '../../src/bin';
+import { main } from '$/bin';
 
 describe('main', () => {
   it('should be runnable', () => {
     expect(() => {
       main();
-    }).not.to.throw();
+    }).not.toThrowError();
   });
 });
