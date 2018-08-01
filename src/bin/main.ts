9b5dc44b2380671d0795f82ddd76f02dce31014d
Junyoung Clare Jang <jjc9310@gmail.com>
2018-08-01 07:45:52 -0400

    refactor(lib): rename create project to create package.json

--- src/bin/main.ts
+++ src/bin/main.ts
@@ -1,9 +1,9 @@
-import { createProject } from '../lib';
+import { createPackageJson } from '../lib';
 
 import { printLogo } from './printLogo';
 
 export const main = async () => {
   await printLogo();
 
-  createProject();
+  createPackageJson();
 };
