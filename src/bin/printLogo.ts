324cff3f96c6a74904fe44edb9f15985b0c6cc65
Junyoung Clare Jang <jjc9310@gmail.com>
2018-08-04 01:40:24 -0400

    feat(cli): make printLogo function to use the name in package.json

--- src/bin/printLogo.ts
+++ src/bin/printLogo.ts
@@ -3,11 +3,14 @@ import { promisify } from 'util';
 import figlet from 'figlet';
 import gradient from 'gradient-string';
 
+import packageJson from '../../package.json';
+
 export const printLogo = async () => {
-  const asciiLogo = await promisify<string, string | undefined>(figlet)('npm-starter');
+  const packageName = packageJson.name;
+  const asciiLogo = await promisify<string, string | undefined>(figlet)(packageName);
 
   if (asciiLogo === undefined) {
-    console.log(gradient.pastel('npm-starter'));
+    console.log(gradient.pastel(packageName));
     return;
   }
 
