c3a99a6727b250eab68f4e4cc16ef51ca086ed88
Junyoung Clare Jang <jjc9310@gmail.com>
2018-08-04 01:38:41 -0400

    chore(package): fix typings to use packaged one

--- src/bin/printLogo.ts
+++ src/bin/printLogo.ts
@@ -4,6 +4,12 @@ import figlet from 'figlet';
 import gradient from 'gradient-string';
 
 export const printLogo = async () => {
-  const asciiLogo = await promisify<string, string>(figlet)('npm-starter');
+  const asciiLogo = await promisify<string, string | undefined>(figlet)('npm-starter');
+
+  if (asciiLogo === undefined) {
+    console.log(gradient.pastel('npm-starter'));
+    return;
+  }
+
   console.log(gradient.pastel.multiline(asciiLogo));
 };
