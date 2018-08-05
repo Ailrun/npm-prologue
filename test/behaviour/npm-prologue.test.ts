ec2768a05b9bfa4a75654963eed6d0201548372c
Junyoung Clare Jang <jjc9310@gmail.com>
2018-08-04 22:34:50 -0400

    test(cli): update cli test to use util functions

--- test/behaviour/npm-prologue.test.ts
+++ test/behaviour/npm-prologue.test.ts
@@ -1,23 +1,30 @@
-import { spawn } from 'child_process';
+import { ChildProcess, spawn } from 'child_process';
 import { join } from 'path';
 import { promisify } from 'util';
 
 import rimraf = require('rimraf');
 
-import { packageRoot } from './test-utils';
+import { exitCode, packageRoot, stdoutData } from './test-utils';
 
 const execPath = join(packageRoot, 'dist/npm-prologue.js');
 
 describe('npm-prolgue', () => {
+  let npmPrologue: ChildProcess;
+
+  afterEach(async () => {
+    npmPrologue.kill();
+    await promisify(rimraf)(join(__dirname, '__temp__/*'));
+  });
+
   it('should be able to run with normally', async () => {
-    const npmPrologue = spawn(execPath, [], {
+    npmPrologue = spawn(execPath, [], {
       stdio: 'pipe',
       cwd: join(__dirname, '/__temp__'),
     });
-    const logo = await new Promise<Buffer>((resolve) => npmPrologue.stdout.once('data', resolve));
+    const logo = await stdoutData(npmPrologue.stdout);
     expect(logo).toMatchSnapshot();
-    const code = await new Promise<number>((resolve) => npmPrologue.once('exit', resolve));
+
+    const code = await exitCode(npmPrologue);
     expect(code).toBe(0);
-    await promisify(rimraf)(join(__dirname, '__temp__/*'));
   });
 });
--- test/behaviour/test-utils.ts
+++ test/behaviour/test-utils.ts
@@ -1,4 +1,6 @@
+import { ChildProcess } from 'child_process';
 import { join, resolve } from 'path';
+import { Readable, Writable } from 'stream';
 
 export const packageRoot = resolve(join(__dirname, '../../'));
 
@@ -18,3 +20,24 @@ export const resetMemoryFs: (
     }
   }
 };
+
+export const stdoutData = (stdout: Readable) => {
+  return new Promise<Buffer>((resolve) => {
+    stdout.once('data', resolve);
+  })
+    .then((buf) => buf.toString('utf-8'));
+};
+
+export const stdinWrite = (stdin: Writable, str: string) => {
+  return new Promise<void>((resolve) => {
+    stdin.write(str, resolve);
+  });
+};
+
+export const stdinWriteLn = (stdin: Writable, str: string) => {
+  return stdinWrite(stdin, str + '\n');
+};
+
+export const exitCode = (process: ChildProcess) => {
+  return new Promise<number>((resolve) => process.once('exit', resolve));
+}
